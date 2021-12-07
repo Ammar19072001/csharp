0a40e0268bb0c3a975798a5a46c8153abe82d1a5
Ammar19072001 <ammar.rajput101@gmail.com>
2021-12-07 18:53:32 +0500

    ifelse condition in c#

--- hello.cs
+++ hello.cs
@@ -5,31 +5,21 @@ namespace hell
     {
         public static void Main(string[] args)
         {
-            char character;
-            Console.WriteLine("Enter any Varb");
-            character = Convert.ToChar(Console.ReadLine().ToLower());
-            switch(character)
+            Console.WriteLine("Enter a Number");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a>0)
             {
-                case 'a':
-                Console.WriteLine("This is a Vowel");
-                break;
-                case 'e':
-                Console.WriteLine("This is a Vowel");
-                break;
-                case 'i':
-                Console.WriteLine("This is a Vowel");
-                break;
-                case 'o':
-                Console.WriteLine("This is a Vowel");
-                break;
-                case 'u':
-                Console.WriteLine("This is a Vowel");
-                break;
-                default:
-                Console.WriteLine("This is a Constant");
-                break;
-
+                Console.WriteLine("The value is Postive");
+            }
+            else if(a<0)
+            {
+                Console.WriteLine("The value is Negative");
+            }
+            else if(a==0)
+            {
+                Console.WriteLine("The Value is Zero");
             }
         }
-    }    
+    }
 }
