984fbfe12c39e4d65a369781ea92057f447e118c
Ammar19072001 <ammar.rajput101@gmail.com>
2021-12-07 18:39:05 +0500

    Mycode

---
+++ Book.cs
@@ -0,0 +1,10 @@
+namespace csharp
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+
+// namespace csharp
+// {
+//     class Program
+//     {
+//      public class Book
+//       {
+//         public string title;
+//         public string author;
+//         public int pages;
+//     }
+//         static void Main(string[] args)
+//         {
+//             Book book1 = new Book();
+//             book1.title = "Harry Potter";
+//             book1.author = "JK rowling";
+//             book1.pages=1000;
+//             Console.WriteLine(book1.author);
+//             Console.WriteLine(book1.pages);
+//             Console.ReadLine();
+//         }
+//     }
+// }
